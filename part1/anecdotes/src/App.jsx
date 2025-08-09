75f314ac699a9479a184b738fff9180785f52917
rtom03 <zuckm709@gmail.com>
2025-08-09 06:44:59 +0100

    implement anecdote project

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [anecVotes, setAnecVotes] = useState(Array(anecdotes.length).fill(0));
+
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  }
+
+  const handleNext = () => {
+    const randomIndex = getRandomInt(0, anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVotes = () => {
+    const newAnecVotes = [...anecVotes];
+    newAnecVotes[selected] += 1;
+    setAnecVotes(newAnecVotes);
+  };
+
+  const maxVote = Math.max(...anecVotes);
+  const maxAnecdoteVote = anecVotes.indexOf(maxVote);
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <p>{anecVotes[selected]}</p>
+      <button onClick={handleNext}>next anecdote</button>
+      <button onClick={handleVotes}>vote</button>
+      <hr />
+      {maxAnecdoteVote !== 0 && (
+        <>
+          <p>{anecdotes[maxAnecdoteVote]}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
