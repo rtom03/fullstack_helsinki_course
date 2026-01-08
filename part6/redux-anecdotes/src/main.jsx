f37c7cce80d21e6a9e631cf968af9f1096e14bbc
Tom <zuckm709@gmail.com>
2026-01-08 14:00:36 +0100

    implement exercise 6-23---24

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,13 +3,16 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./reducers/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NotificationContextProvider } from "./context/NotificationContext";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <App />
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
     </Provider>
   </QueryClientProvider>
 );
